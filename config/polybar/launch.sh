96c273b63e1f8eec004e386f095cef00a1e48507
laluka <loukajc@gmail.com>
2026-02-19 11:04:41 +0100

    polybar-fix-for-skilo

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
-set -euo pipefail
 
 # Terminate already running bar instances
 killall -q polybar || true
@@ -8,11 +7,11 @@ killall -q polybar || true
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 MONITORS=$(polybar --list-monitors)
-MONITOR_PRIMARY=$(echo "$MONITORS" | grep "primary" | cut -d":" -f1)
-MONITOR_EXTRA=$(echo "$MONITORS" | grep -v "primary" | cut -d":" -f1)
+MONITOR_PRIMARY=$(echo "$MONITORS" | grep "primary" | cut -d":" -f1 || true)
+MONITOR_EXTRA=$(echo "$MONITORS" | grep -v "primary" | cut -d":" -f1 || true)
 
 # Start main polybar with tray
-MONITOR=$MONITOR_PRIMARY MOD_RIGHT="systray microphone pulseaudio memory cpu filesystem battery wlan eth date" polybar main &
+MONITOR=${MONITOR_PRIMARY:-${MONITOR_EXTRA%% *}} MOD_RIGHT="systray microphone pulseaudio memory cpu filesystem battery wlan eth date" polybar main &
 for m in $MONITOR_EXTRA; do
     MONITOR=$m MOD_RIGHT="microphone pulseaudio memory cpu filesystem battery wlan eth date" polybar main &
 done
